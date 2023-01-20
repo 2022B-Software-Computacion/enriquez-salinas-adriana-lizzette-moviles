fd5f561fdfa03cc1681021cb393e50f1be122cfa
Adriana Enríquez <adriana.enriquez@epn.edu.ec>
2023-01-19 23:20:22 -0500

    Operaciones
    actualizar y write update data

--- Examen/src/main/kotlin/OperacionesGenero.kt
+++ Examen/src/main/kotlin/OperacionesGenero.kt
@@ -91,7 +91,115 @@ class OperacionesGenero {
         return listGenero
     }
 
+    //Actualizar
+    fun actualizarGenero(findGenero: String, listGenero: ArrayList<Genero>, pathFile: String):
+            ArrayList<Genero>{
+        try {
+            for (generoFind in listGenero){
+                if (generoFind.nombreGenero == findGenero){
+                    val index = listGenero.indexOf(generoFind)
+                    println("*** Información sobre el genero  "+"\n")
+                    println("1. Nombre del género: "+generoFind.nombreGenero)
+                    println("2. Tiene cámara libre?: "+generoFind.camaraLibre)
+                    println("3. Subgenero: "+generoFind.subgenero)
+                    println("4. FPS: "+generoFind.fps)
+                    println("5. Es online?: "+generoFind.online)
+                    println("Seleccione la información deseas cambiar: ")
+                    when (readLine()!!.toInt()){
+                        1 -> {
+                            println("Ingrese la nueva información de nombre:")
+                            val newName = readLine()
+                            generoFind.nombreGenero = newName.toString()
+                            listGenero[index] = generoFind
+                            writeUpdateData(listGenero, pathFile)
+                            println("El nombre del videojuego se ha actualizado con éxito")
+                            break
+                        }
+                        2 -> {
+                            println("Ingrese la nueva información sobre la camara libre:")
+                            val camara = readLine()!!.toBoolean()
+                            generoFind.camaraLibre = camara
+                            listGenero .set(index,generoFind)
+                            writeUpdateData(listGenero, pathFile)
+                            println("La actualización del estado sobre la camara se ha actualizado con exito!")
+                            break
+                        }
+                        3 -> {
+                            println("Ingrese la nueva información del subgenero de videojuego:")
+                            val subgenero = readLine()
+                            generoFind.subgenero = subgenero.toString()
+                            listGenero.set(index,generoFind)
+                            writeUpdateData(listGenero, pathFile)
+                            println("El subgenero se ha actualizado con éxito")
+                            break
+                        }
+                        4 -> {
+                            println("Ingrese la nueva información sobre los fps")
+                            val fps = readLine()!!
+                            generoFind.fps = fps.toInt()
+                            listGenero.set(index,generoFind)
+                            writeUpdateData(listGenero, pathFile)
+                            println("La información de fps se ha actualizado con éxito")
+                            break
+                        }
+                        5 -> {
+                            println("Ingrese la nueva información sobre si es online:")
+                            val online = readLine()!!.toBoolean()
+                            generoFind.camaraLibre = online
+                            listGenero .set(index,generoFind)
+                            writeUpdateData(listGenero, pathFile)
+                            println("La actualización del estado sobre si es online o no se ha actualizado con exito!")
+                            break
+                        }
+                        else -> {
+                            println("La operación de actualización escogida no existe")
+                            break
+                        }
+                    }
+                }else{
+                    println("El videojuego ingresado no existe.")
+                }
+            }
+        }catch (e:Exception){
+            println("Error Update $e")
+        }
+        return listGenero
+    }
 
+    fun writeUpdateData(listGenero: ArrayList<Genero>, pathFile: String) {
+        try {
+            var file: File? = null
+            var fw: FileWriter? = null
+            var pw: PrintWriter? = null
+            var text = ""
+            for (genero in listGenero){
+                try {
+                    file = File(pathFile)
+                    fw = FileWriter(file)//true
+                    pw = PrintWriter(fw)
+                    text = text + genero.nombreGenero + ",";
+                    text = text + genero.camaraLibre + ",";
+                    text = text + genero.subgenero+ ",";
+                    text = text + genero.fps+ ",";
+                    text = text + genero.online+"\n";
+                    fw.write(text);
+                    fw.write("\n");
+                }catch (e: Exception){
+                    println("Error Write Update Genero $e")
+                }finally {
+                    try {
+                        if(fw !=null){
+                            fw.close()
+                        }
+                    }catch (e: Exception){
+                        println("Error Write Update Genero $e")
+                    }
+                }
+            }
+        }catch (e: Exception){
+            println("Error Update $e")
+        }
+    }
 
 
 
